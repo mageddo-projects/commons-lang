d39d715b89fcaac3e007facfceae7b763100cd5f
Elvis de Freitas <edigitalb@gmail.com>
2023-03-18 00:31:37 -0300

    new cache methods

--- src/main/java/com/mageddo/commons/caching/internal/Wrapper.java
+++ src/main/java/com/mageddo/commons/caching/internal/Wrapper.java
@@ -7,18 +7,18 @@ import java.time.temporal.ChronoUnit;
 import lombok.Value;
 
 @Value
-  public class Wrapper {
+public class Wrapper {
 
-    private final Object value;
-    private final LocalDateTime createdAt;
-    private final Duration ttl;
+  private final Object value;
+  private final LocalDateTime createdAt;
+  private final Duration ttl;
 
-    public static Wrapper of(Object value, Duration ttl) {
-      return new Wrapper(value, LocalDateTime.now(), ttl);
-    }
-
-    public boolean hasExpired() {
-      final long millis = ChronoUnit.MILLIS.between(this.createdAt, LocalDateTime.now());
-      return this.ttl.compareTo(Duration.ofMillis(millis)) <= 0;
-    }
+  public static Wrapper of(Object value, Duration ttl) {
+    return new Wrapper(value, LocalDateTime.now(), ttl);
   }
+
+  public boolean hasExpired() {
+    final long millis = ChronoUnit.MILLIS.between(this.createdAt, LocalDateTime.now());
+    return this.ttl.compareTo(Duration.ofMillis(millis)) <= 0;
+  }
+}
